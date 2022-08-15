926ce209a166125de97198e2c65a92e7f3a48f9f
Kabarakh <git@kabarakh.de>
2022-08-15 11:56:16 +0200

    TASK: display entrances by exit and order both views

---
+++ src/components/ExitAndEntrance.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps(['entrance'])
+
+const displayEntrance = ref(false);
+
+const toggleDisplay = () => {
+  displayEntrance.value = !displayEntrance.value;
+};
+
+</script>
+
+<template>
+  <span @click="toggleDisplay()">{{props.entrance.to}}</span>
+  <span v-if="displayEntrance"> &lt;- {{props.entrance.from}}</span>
+</template>
--- src/views/EntranceShuffleView.vue
+++ src/views/EntranceShuffleView.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
 import { useDataStore } from "@/stores/data";
 import EntranceAndExit from "@/components/EntranceAndExit.vue";
+import ExitAndEntrance from "@/components/ExitAndEntrance.vue";
 import router from "@/router";
+import { Entrance } from "@/models/Entrance";
 
 const store = useDataStore();
 if (!store.seed) {
   router.push('/');
 }
+
+const orderedByEntrance = [...store.entrances].sort((a: Entrance, b: Entrance) => {
+  return a.from.localeCompare(b.from, undefined, {sensitivity: "accent"});
+});
+
+const orderedByExit = [...store.entrances].sort((a: Entrance, b: Entrance) => {
+  return a.to.localeCompare(b.to, undefined, {sensitivity: "accent"});
+});
 </script>
 
 <template>
-  <h2>Entrances View</h2>
+  <h2>Entrance Shuffle View</h2>
+  <h3>Entrance View</h3>
   <p>Click an entrance name to reveal the target</p>
-  <div :key="key" v-for="(entrance, key) in store.entrances">
+  <div :key="key" v-for="(entrance, key) in orderedByEntrance">
     <EntranceAndExit :entrance="entrance"/>
+  </div>
+
+  <h3>Exit View</h3>
+  <p>Click an exit name to reveal the entrance for it (e.g. if you know where an item is)</p>
+  <div :key="key" v-for="(entrance, key) in orderedByExit">
+    <ExitAndEntrance :entrance="entrance"/>
   </div>
 </template>
